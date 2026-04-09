dcd1de050cc2c5df3dd9500e090ee4384071ab8d
Claire Lee <jclairelee2@gmail.com>
2026-04-08 17:53:43 -0700

    Fix AutoLayout issue and connect navigation to TaskListViewController

--- TodoApp/TaskListViewController.swift
+++ TodoApp/TaskListViewController.swift
@@ -1,6 +1,27 @@
 import UIKit
 
-class TaskListViewController: UIViewController {
+class TaskListViewController: UIViewController, UITableViewDataSource {
+
     @IBOutlet weak var tableView: UITableView!
 
+    var tasks: [String] = ["Test Task 1", "Test Task 2"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+
+        cell.textLabel?.text = tasks[indexPath.row]
+
+        return cell
+    }
 }
