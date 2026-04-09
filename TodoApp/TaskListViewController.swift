d6494a6563afcedb3d57a1a4c7563c1a81b06646
Claire Lee <jclairelee2@gmail.com>
2026-04-08 19:55:20 -0700

    Implement Core Data save, fetch tasks and navigation

--- TodoApp/TaskListViewController.swift
+++ TodoApp/TaskListViewController.swift
@@ -1,22 +1,49 @@
 import UIKit
+import CoreData
 
 class TaskListViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
-
-    var tasks: [String] = ["Test Task 1", "Test Task 2"]
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var tasks: [String] = []
     var newTask: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.dataSource = self
-        
-        if let task = newTask {
-               tasks.append(task)
-           }
+
+        if let task = newTask, !task.isEmpty {
+
+            let newTaskItem = Task(context: context)
+            newTaskItem.title = task
+
+            do {
+                try context.save()
+            } catch {
+                print("Error saving")
+            }
+        }
+
+        fetchTasks()
     }
 
+    func fetchTasks() {
+
+        do {
+            let items = try context.fetch(Task.fetchRequest())
+
+            tasks = items.map { $0.title ?? "" }
+
+            tableView.reloadData()
+
+        } catch {
+            print("Error fetching")
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
     }
